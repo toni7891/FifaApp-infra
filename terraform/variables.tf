7ab18dfb2a15c58ca606467a2ecf02edd5d77d95
toni7891 <toniv7891@gmail.com>
2026-07-05 14:41:45 +0300

    Use free-tier eligible t3.small for EKS node group

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -19,7 +19,7 @@ variable "cluster_version" {
 variable "node_instance_type" {
   description = "EC2 instance type for EKS nodes"
   type        = string
-  default     = "t3.medium"
+  default     = "t3.small"
 }
 
 variable "desired_nodes" {
